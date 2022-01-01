89429a49f78cf5f4e20c9d91c27c6e990f2b0c9f
TalhaKhaliq007 <96942781+TalhaKhaliq007@users.noreply.github.com>
2022-01-01 18:00:35 +0500

    Delete Qno1.py

--- Assignment # 1/Qno1.py
+++
@@ -1,9 +0,0 @@
-def display():
-    print('Twinkle,twinkle,little star,')
-    print("        " 'How I wonder what you are!')
-    print("                " 'Up above the world so high,')
-    print("                " 'Like a diamond in the sky.')
-    print('Twinkle,twinkle,little star,')
-    print("        " 'How I wonder what you are!')
-
-display()
